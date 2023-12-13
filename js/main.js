c6a491182304c4aa475894f8b204f18a01da29d6
YuriiTurok <yuriiklontsak@ukr.net>
2023-12-13 20:50:07 +0200

    delete scroll in addpative header

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 let burgerBtn = document.querySelector(".header__burger-btn");
 let menuList = document.querySelector(".header__menu-list");
+const body = document.querySelector("body");
 
 burgerBtn.addEventListener('click', function() {
   if (burgerBtn.classList.contains('active')) {
     burgerBtn.classList.remove('active');
     menuList.classList.remove('active');
+    body.classList.remove('menu__mobile-opened');
   }
   else {
     burgerBtn.classList.add('active');
     menuList.classList.add('active');
+    body.classList.add('menu__mobile-opened')
   }
 });
+
+/* ------------------------------------------ */
+
+
