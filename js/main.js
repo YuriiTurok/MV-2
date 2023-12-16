784d20c04688d5c64a2022d0077cb45b1487c730
YuriiTurok <yuriiklontsak@ukr.net>
2023-12-16 03:16:11 +0200

    add section 'demo worlds'

--- js/main.js
+++ js/main.js
@@ -18,3 +18,40 @@ burgerBtn.addEventListener('click', function() {
 /* ------------------------------------------ */
 
 
+const sliderItem = document.querySelector('.slider__item');
+const sliderLine = document.querySelector('.demo__slider-line');
+const btnPrev = document.querySelector('.demo__slider-btn-prev');
+const btnNext = document.querySelector('.demo__slider-btn-next');
+
+let sliderCount = 0;
+let sliderWidth;
+
+btnNext.addEventListener('click', nextSlide);
+btnPrev.addEventListener('click', prevSlide);
+
+function showSlide() {
+  sliderWidth = sliderItem.offsetWidth;
+  sliderLine.style.width = sliderWidth * sliderItem.length + 'px';
+
+  rollSlider();
+}
+showSlide();
+
+
+function nextSlide() {
+  sliderCount++;
+  if (sliderCount > 2) sliderCount = 2;
+
+  rollSlider();
+}
+
+function prevSlide() {
+  sliderCount--;
+  if (sliderCount < -2) sliderCount = -2;
+
+  rollSlider();
+}
+
+function rollSlider() {
+  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
